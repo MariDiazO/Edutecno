31f332e4d3434424c38445604c2cf9bedbbc5fd5
mari <mari.diaz.di@outlook.com>
2023-04-14 22:34:57 -0400

    guardado

--- Práctica de consolidación/Práctica de consolidaciónM2-MJDíaz/Js/javascript.js
+++ Práctica de consolidación/Práctica de consolidaciónM2-MJDíaz/Js/javascript.js
@@ -10,22 +10,20 @@ fetch('https://digimon-api.vercel.app/api/digimon')
 .then(response => response.json())
 .then(data=>{
     console.log(data);
-    mostrartabla(response);
+    mostrartabla(data);
 }).catch(error => console.log(error))
 
 //funcion obtener datos digimon y mostrarlo en id=Tabla id=Contenido
 function mostrartabla(response){
-
-//limpiar el elemento con id contenido
-contenido.innerHTML = "";
-
-//recorrer datos digimon de la api
-for(let temp of response){
-    contenido.innerHTML += // ` esta comilla francesa indica que html lea los datos temporales
-    `<td>${temp.name}</td>
-    <td>${temp.level}</td> 
-    <td><img src="${temp.img}" alt"" width ="50px" heigth="50px"></td>
-    `
-    }
+    //limpiar el elemento con id contenido
+    contenido.innerHTML = "";
+    //recorrer datos digimon de la api
+    for(let temp of response){
+        contenido.innerHTML += // ` esta comilla francesa indica que html lea los datos temporales
+        `<td>${temp.name}</td>
+        <td>${temp.level}</td> 
+        <td><img src="${temp.img}" alt"" width ="50px" heigth="50px"></td>
+        `
+        }
 
 }
