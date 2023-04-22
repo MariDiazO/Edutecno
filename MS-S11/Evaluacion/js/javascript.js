86ad796e788fb8a5de8eca15e3fad46323a25375
mari <mari.diaz.di@outlook.com>
2023-04-22 13:34:25 -0400

    Cambiando nombre repositorio

--- MS-S11/Evaluacion/js/javascript.js
+++ MS-S11/Evaluacion/js/javascript.js
@@ -12,43 +12,9 @@ $(document).ready(function(){
         //validar si el elemento tiene datos o valores y si no asignar una pelicula por default
         if(busqueda.trim() === "" || busqueda == null){
             busqueda = "Castle in the sky"
-        }
-        
-        //Fetch api
-        fetch(`https://studio-ghibli-films-api.herokuapp.com/films`)
-            .then(response => response.json())//se obtiene el json de la respuesta
-            .then(data => {
-                console.log(data);
-                if (data.length > 0) {
-                    tarjeta(data[0]);
-                } else {
-                    console.log("No se encontró ninguna película con ese título.");
-                }
-             }).catch(error => console.log(error));
-
-             //funcion para inyectar datos en la tarjeta
-             function tarjeta(pelicula) {
-            var img = document.createElement("img")
-            img.classList.add("card-img-top")
-            img.src = pelicula.image
-            divCard.appendChild(img)
-            var titulo = document.createElement("h5")
-            titulo.classList.add("card-title")
-            titulo.textContent = pelicula.title
-            divCardBody.appendChild(titulo)
-            var descripcion = document.createElement("p")
-            descripcion.classList.add("card-text")
-            descripcion.textContent = pelicula.description
-            divCardBody.appendChild(descripcion)
-            var fechaLanzamiento = document.createElement("p")
-            fechaLanzamiento.classList.add("card-text")
-            fechaLanzamiento.innerHTML = "<strong>Fecha de lanzamiento:</strong> " + pelicula.release_date
-            divCardBody.appendChild(fechaLanzamiento)
-            var director = document.createElement("p")
-            director.classList.add("card-text")
-            director.innerHTML = "<strong>Director:</strong> " + pelicula.director
-            divCardBody.appendChild(director)
-            }
+        }   
     });
-
+        //fetch api
 });
+
+
--- Práctica de consolidación/Práctica de consolidaciónM2-MJDíaz/Js/javascript.js
+++ Práctica de consolidación/Práctica de consolidaciónM2-MJDíaz/Js/javascript.js
@@ -2,6 +2,8 @@
 let tabla= document.getElementById("tabla");
 let contenido= document.getElementById("contenido");
 let carta = document.getElementById("carta");
+let digicards = document.getElementById("digicards");
+
 //consumir API con fetch
 fetch('https://digimon-api.vercel.app/api/digimon')
 .then(response => response.json())
@@ -12,11 +14,11 @@ fetch('https://digimon-api.vercel.app/api/digimon')
 
 //funcion obtener datos digimon y mostrarlo en id=Tabla id=Contenido
 function mostrarTabla(response){
-    
+    document.getElementById("carta").style.display = "none";
+    document.getElementById("digicards").style.display = "none";
     //limpiar el elemento con id contenido
     contenido.innerHTML = "";
     //recorrer datos digimon de la api
-    
     for(let temp of response){
         contenido.innerHTML += // ` esta comilla francesa indica que html lea los datos temporales
         `<td>${temp.name}</td>
@@ -24,11 +26,23 @@ function mostrarTabla(response){
         <td><img src="${temp.img}" alt"" width ="60px" heigth="60px"></td>
         `
         }
-   }
+}
 
 //Funcion para obtener los datos del digimon y mostrar el div = carta
 //Utilizar fetch y el id busqueda en el input para obtener los datos en la carta
 function mostrarCarta(){
+    tabla = document.getElementById("tabla")
+    contenido = document.getElementById("contenido")
+    carta = document.getElementById("carta")
+    digicards = document.getElementById("digicards")
+
+    if(carta != null && tabla !=null && contenido !=null && digicards !=null){
+    tabla.style.display = "none";
+    contenido.style.display = "none";
+    carta.style.display = "block";
+    digicards.style.display = "none";
+    }
+
     let busqueda=document.getElementById("busqueda").value.toLowerCase();
     //Funcion api con fetch
     fetch(`https://digimon-api.vercel.app/api/digimon/name/${busqueda}`)
@@ -37,27 +51,59 @@ function mostrarCarta(){
             console.log(data);
             construirCarta(data); // invocando la funcion construirCarta(data)
         }).catch(error => console.log(error))
-   }
+}
    //funcion para crear la carta del digimon dinamicamente
     function construirCarta(params) {
-        //ocultar id ="tabla" y "body" 
-        document.getElementById("tabla").style.display = "none";
-        document.getElementById("contenido").style.display = "none";
-    
     //limpiar el elemento con id contenido
-    tabla.innerHTML = "";
-    contenido.innerHTML = "";
     carta.innerHTML = "";
+    //tabla.innerHTML = "";
+    //contenido.innerHTML = "";
+    //digicards.innerHTML = "";
     //recorrer datos digimon de la api
     for(let temp of params){
         carta.innerHTML += 
         `<div class="card col-sm-12 col-md-6 mx-auto" style="width: 18rem;">
-            <img src="${temp.img}" class="card-img-top" alt="...">
-            <div class="card-body">
-            <h5 class="card-title">${temp.name}</h5>
-            <p class="card-text">Nivel: ${temp.level}</p>
-            </div>
+        <img class="card-img-top" src="${temp.img}" alt="Imagen" style="width:100%">
+        <div class="card-body">
+        <div class="card-title"><h4><b>${temp.name}</b></h4></div>
+        <div class="card-text"><p>Nivel: ${temp.level}</p></div>
         </div>`
-         
         }
     }
+    //Mostrar todas las cartas
+    function mostrarCartas(){
+        tabla = document.getElementById("tabla")
+        contenido = document.getElementById("contenido")
+        carta = document.getElementById("carta")
+        digicards = document.getElementById("digicards")
+    
+        if(carta != null && tabla !=null && contenido !=null && digicards !=null){
+        tabla.style.display = "none";
+        contenido.style.display = "none";
+        carta.style.display = "none";
+        digicards.style.display = "block";
+        }
+    
+        //Funcion api con fetch
+        fetch(`https://digimon-api.vercel.app/api/digimon`)
+            .then(response => response.json())
+            .then(data=>{
+                console.log(data);
+                construirCartas(data); // invocando la funcion construirCarta(data)
+            }).catch(error => console.log(error))
+    }
+       //funcion para crear la carta del digimon dinamicamente
+        function construirCartas(params) {
+        //limpiar el elemento con id contenido
+        digicards.innerHTML = "";
+        //recorrer datos digimon de la api
+        for(let temp of params){
+            digicards.innerHTML += 
+            `<div class="card col-sm-12 col-md-6 mx-auto" style="width: 18rem;">
+            <img class="card-img-top" src="${temp.img}" alt="Imagen" style="width:100%">
+            <div class="card-body">
+            <div class="card-title"><h4><b>${temp.name}</b></h4></div>
+            <div class="card-text"><p>Nivel: ${temp.level}</p></div>
+            </div>`
+            }
+        }
